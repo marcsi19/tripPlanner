5a42ad593830a99294642c679f0dd9d3c3891861
Mariann M <mariann19@gmail.com>
2018-09-25 16:20:15 -0400

    map is ready

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 console.log("Hello from JavaScript");
+const mapboxgl = require("mapbox-gl");
+
+mapboxgl.accessToken = 'pk.eyJ1IjoibWFyY3Npa2EiLCJhIjoiY2ptaTV1eWRuMDFsYjNrcXpwMDAyc3MwMCJ9.E9HrrJFwKI1hgzPaaIPKmA';
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  zoom: 12, // starting zoom
+  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+});
