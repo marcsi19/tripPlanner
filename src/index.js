12790302df91167ccc8072f8082fba5e123e2b01
Mariann M <mariann19@gmail.com>
2018-09-25 17:10:12 -0400

    havingfun

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ console.log("Hello from JavaScript");
 const mapboxgl = require("mapbox-gl");
 const buildMarker = require('./marker');
 
-const marker = buildMarker("activity", [-87.6354, 41.8885]); // or 
-marker.addTo(map);
 
 mapboxgl.accessToken = 'pk.eyJ1IjoibWFyY3Npa2EiLCJhIjoiY2ptaTV1eWRuMDFsYjNrcXpwMDAyc3MwMCJ9.E9HrrJFwKI1hgzPaaIPKmA';
 
@@ -20,3 +18,5 @@ markerDomEl.style.height = "39px";
 markerDomEl.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)";
 
 new mapboxgl.Marker(markerDomEl).setLngLat([-74.009, 40.705]).addTo(map); // [-87.641, 41.895] for Chicago
+const marker = buildMarker("restaurants", [-87.6354, 41.8885]); // or
+marker.addTo(map);
--- src/marker.js
+++ src/marker.js
@@ -1,4 +1,4 @@
-const mapbox = require("mapbox-gl");
+const mapboxgl = require("mapbox-gl");
 
 const iconURLs = {
     hotels: "http://i.imgur.com/D9574Cu.png",
@@ -12,7 +12,8 @@ markerDomEl.style.width = "32px";
 markerDomEl.style.height = "39px";
 markerDomEl.style.backgroundImage = `url(${iconURLs[type]})`;
 
-new mapboxgl.Marker(markerDomEl).setLngLat(coords);
+let coord = new mapboxgl.Marker(markerDomEl).setLngLat(coords);
+return coord;
 };
 
 module.exports = buildMarker;
